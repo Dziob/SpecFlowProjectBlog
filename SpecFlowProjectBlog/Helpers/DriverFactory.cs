190f8f47560b735d122857fd52e2e7f245ed1385
mtoma <mtomaszewska2@gmail.com>
2023-05-24 15:42:19 +0200

    Driver factory

---
+++ SpecFlowProjectBlog/Helpers/DriverFactory.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V111.Runtime;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowProjectBlog.Helpers
+{
+    internal class DriverFactory
+    {
+        public static IWebDriver ReturnDriver (DriverType driverType)
+        {
+            IWebDriver driver;
+            switch (driverType)
+            {
+                case driverType.Chrome:
+                    driver=new ChromeDriver();
+                    break;
+
+                    case driverType.Firefox:
+                    driver=new FirefoxDriver(); 
+                    break;
+
+                case driverType.Edge:
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(driverType), driverType, null);
+            }
+
+            return driver;
+
+        }
+    }
+
+    internal enum DriverType
+    {
+        Chrome,
+        Firefox,
+        Edge,
+    }
+}
---
+++ SpecFlowProjectBlog/Settings/Hooks1.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+
+namespace SpecFlowProjectBlog.Settings
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        private IWebDriver driver;
+
+        [BeforeScenario("@tag1")]
+        public void BeforeScenario()
+        {
+ 
+            driver = DriverFactory
+        }
+
+        [BeforeScenario(Order = 1)]
+        public void FirstBeforeScenario()
+        {
+            
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            
+        }
+    }
+}
