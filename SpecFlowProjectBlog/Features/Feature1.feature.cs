fd3874e884021aaaff4da70deb4c0ce6fd97b52a
mtoma <mtomaszewska2@gmail.com>
2023-05-26 19:57:19 +0200

    steps

--- SpecFlowProjectBlog/Features/Feature1.feature.cs
+++ SpecFlowProjectBlog/Features/Feature1.feature.cs
@@ -103,10 +103,10 @@ this.ScenarioInitialize(scenarioInfo);
             {
                 this.ScenarioStart();
 #line 7
- testRunner.Given("I enter to \'home\' page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+ testRunner.Given("I enter to \"home\" page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
 #line 8
- testRunner.And("I click on ”Contact\" in menu", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.And("I click on \"Contact\" in menu", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 9
  testRunner.When("I fill contact form", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
--- SpecFlowProjectBlog/StepDefinitions/ContactFormSteps.cs
+++ SpecFlowProjectBlog/StepDefinitions/ContactFormSteps.cs
@@ -1,18 +1,78 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TechTalk.SpecFlow;
 
 namespace SpecFlowProjectBlog.StepDefinitions
 {
     internal class ContactFormSteps
     {
-        [Given("I enter to 'home' page")]
-        public void HomePage(string eh)
+        private IWebDriver driver;
+        public ContactFormSteps() {
+            driver = (IWebDriver)ScenarioContext.Current["driver"];
+        }
+
+        [Given(@"I enter to ""(.*)"" page")]
+        public void GivenIEnterToPage(string name)
         {
+            string url = null;
+            if (name == "home")
+                url = "https://courseofautomationtesting.wordpress.com/";
+            driver.Navigate().GoToUrl(url);
 
         }
+
+        [Given(@"I click on ""(.*)"" in menu")]
+        public void GivenIClickInInMenu(string option)
+        {
+            var menuElements = driver.FindElements(By.ClassName("menu-primary-container"));
+
+            switch (option)
+            {
+                case "Home":
+                    menuElements.First(x => x.Text == "Home").Click();
+                    break;
+
+                case "About":
+                    menuElements.First(x => x.Text == "About").Click(); 
+                    break;
+
+                case "Cpntact":
+                    menuElements.First(x => x.Text == "Contact").Click();
+                    Assert.True(driver.Url.Contains("/contact"));
+                    break;
+            }
+        }
+
+        [When(@"I fill contact form")]
+        public void WhenIFillContactForm()
+        {
+            var name = driver.FindElement(By.Id("g3-name"));
+            var email = driver.FindElement(By.Id("g3-email"));
+            var website = driver.FindElement(By.Id("g3-website"));
+            var comment = driver.FindElement(By.Id("contact-form-comment-g3-comment"));
+            var submitBtn = driver.FindElement(By.ClassName("pushbutton-wide"));
+            name.SendKeys("Name");
+            email.SendKeys("test@email.com");
+            website.SendKeys("www.mytest.site");
+            comment.SendKeys("My test comment");
+            submitBtn.Click();
+        }
+
+        [Then(@"I expect to see message as ""(.*)""")]
+        public void ThenIExpectToSeeMessage(string message)
+        {
+            var expectedText = driver.FindElement(By.Id("contact-form-success-header"));
+            Assert.AreEqual(expectedText.Text, message);
+        }
+
+
+
 
     }
 }
